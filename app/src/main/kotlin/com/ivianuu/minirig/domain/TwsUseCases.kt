75f348aede9af1cbe263ac8096e63edd750aee0e
manu <IVIanuu@gmail.com>
2022-06-14 16:24:25 +0200

    ...

--- app/src/main/kotlin/com/ivianuu/minirig/domain/TwsUseCases.kt
+++ app/src/main/kotlin/com/ivianuu/minirig/domain/TwsUseCases.kt
@@ -7,15 +7,11 @@ package com.ivianuu.minirig.domain
 import com.ivianuu.injekt.Provide
 
 @Provide class TwsUseCases(private val remote: MinirigRemote) {
-  suspend fun twsPair(address: String) {
-    remote.withMinirig(address) {
-      send("P")
-    }
+  suspend fun twsPair(address: String) = remote.withMinirig(address) {
+    send("P")
   }
 
-  suspend fun cancelTws(address: String) {
-    remote.withMinirig(address) {
-      send("J")
-    }
+  suspend fun cancelTws(address: String) = remote.withMinirig(address) {
+    send("J")
   }
 }
